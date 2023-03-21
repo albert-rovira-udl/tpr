f33d62a60ed3c97f36d8a69ba4c9a2623b39fa72
diphone-net <43610108+diphone-net@users.noreply.github.com>
2023-03-21 16:22:32 +0100

    second commit

--- app/src/main/java/cat/udl/gtidic/course2223/teacher/tpr/MainActivity.java
+++ app/src/main/java/cat/udl/gtidic/course2223/teacher/tpr/MainActivity.java
@@ -3,6 +3,7 @@ package cat.udl.gtidic.course2223.teacher.tpr;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,5 +14,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         //tmp e
         TextView t;
+        EditText et;
     }
 }
